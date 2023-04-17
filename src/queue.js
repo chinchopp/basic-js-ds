e1335baad34c6c985d79ec3eb26427b3c1b44658
chinchopp <pralgulalex@gmail.com>
2023-04-17 18:05:06 +0300

    feat: add solution

--- src/queue.js
+++ src/queue.js
@@ -13,7 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class listNode {
+  constructor(x){
+    this.value = x
+    this.next = null
+  }
+}
+
+
 class Queue {
+
+  
 
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
